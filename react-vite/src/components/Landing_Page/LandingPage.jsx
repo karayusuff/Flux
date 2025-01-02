938f8077805b20b030b1c11b820ca8a6746a0080
pfelix23 <peter.felix23@gmail.com>
2025-01-02 01:05:18 -0500

    more things

--- react-vite/src/components/Landing_Page/LandingPage.jsx
+++ react-vite/src/components/Landing_Page/LandingPage.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux';
 import { FaRegHeart } from "react-icons/fa";
+import { FaRegCommentDots } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
 import './LandingPage.css';
 function LandingPage() {
     const [posts, setPosts] = useState([]);
@@ -9,6 +11,7 @@ function LandingPage() {
     const [users, setUsers] = useState();
     const [errors, setErrors] = useState();
     const [view, setView] = useState('all');
+    const [fillHeart, setFillHeart] = useState('');
     const navigate = useNavigate();
     const sessionUser = useSelector((state) => state.session.user)
 
@@ -55,6 +58,15 @@ function LandingPage() {
         setView(viewType)
     }
 
+    const fill_heart = (postId) => {
+        setFillHeart(prev => ({
+            ...prev,
+            [postId]: !prev[postId]
+        }))
+    }
+
+    const heart = (postId) => fillHeart[postId] ? <FaHeart /> : <FaRegHeart />
+
     const display = sessionUser ? (view === "all" ? posts : sessionPosts) : posts;
 
     return (
@@ -82,17 +94,23 @@ function LandingPage() {
                         return user.username
                       }
                     }
+
+                    const isFollowing = sessionPosts.some((sessionPost) => sessionPost.id === post.id);
+
                     return (
-                        <picture onClick={handleClick} key={post.id} className='post_container'>
-                            <div className='user_info'>{handleUser()} {sessionUser && sessionUser.id !== post.user_id &&(<div className='follow_text'>Follow</div>)}</div>
-                            <img src={post.image} alt={post.description} className='posts_img' />
+                        <picture key={post.id} className='post_container'>
+                            <div className='user_info'>{handleUser()} {sessionUser && sessionUser.id !== post.user_id  && !isFollowing && (<div className='follow_text'>Follow</div>)}</div>
+                            <img src={post.image} alt={post.description} className='posts_img' onClick={handleClick} />
                             <div className='added_info_div'>
                             <div className='description'>{post.description}</div>
                             <div className='likes_container'> 
-                            <div className='heart_icon'><FaRegHeart /></div>
+                            <div className='heart_icon' onClick={() => fill_heart(post.id)}>{heart(post.id)}</div>
                             <div className='likes_count'>{post.likes}</div>
                             </div>
-                            <div className='added_info'>{post.comment_count}</div>
+                            <div className='comment_container'>
+                            <div className='comment_icon'><FaRegCommentDots /></div>
+                            <div className='comment_count'>{post.comment_count}</div>
+                            </div>
                             </div>
                         </picture>
                     );
--- react-vite/src/components/PostPage/PostPage.jsx
+++ react-vite/src/components/PostPage/PostPage.jsx
@@ -2,11 +2,15 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { FaRegHeart } from "react-icons/fa";
+import { FaRegCommentDots } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
 import './PostPage.css';
 
 function PostPage() {
     const [post, setPost] = useState([]);
     const [errors, setErrors] = useState();
+    const [fillHeart, setFillHeart] = useState('');
     const navigate = useNavigate();
     const post_id_object = useParams()
     const post_id = post_id_object.post_id
@@ -25,6 +29,15 @@ function PostPage() {
         })
     }, []);
 
+    const fill_heart = (postId) => {
+        setFillHeart(prev => ({
+            ...prev,
+            [postId]: !prev[postId]
+        }))
+    }
+
+    const heart = (postId) => fillHeart[postId] ? <FaHeart /> : <FaRegHeart />
+
     return (
         <div className='post_section'>
             <section className='post_section_2'>
@@ -32,8 +45,14 @@ function PostPage() {
                     <img src={post.image} alt={post.description} className='post_img' />
                     <div className='added_info_container'>
                     <div className='post_description'>{post.description}</div>
-                    <div className='post_added_info'>{post.likes}</div>
-                    <div className='post_added_info'>{post.comment_count}</div>
+                    <div className='likes_container'> 
+                    <div className='heart_icon' onClick={() => fill_heart(post.id)}>{heart(post.id)}</div>
+                    <div className='likes_count'>{post.likes}</div>
+                    </div>
+                    <div className='comment_container'>
+                    <div className='comment_icon'><FaRegCommentDots /></div>
+                    <div className='comment_count'>{post.comment_count}</div>
+                    </div>
                     </div>
                 </picture>                   
             </section>
